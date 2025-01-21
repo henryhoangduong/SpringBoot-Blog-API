cdd422fbc71c6124405ad4e36c4f85ca5f8fbe13
henryhoangduong <henryhoangduong@gmail.com>
2025-01-21 12:59:28 +0700

    create post entity

---
+++ src/main/java/com/example/springboot_blog_rest_api/entity/Post.java
@@ -0,0 +1,17 @@
+package com.example.springboot_blog_rest_api.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String content;
+}
