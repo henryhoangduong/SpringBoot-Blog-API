cf068940e819c0e08c3ac2773613af251278d5e2
henryhoangduong <henryhoangduong@gmail.com>
2025-02-05 21:40:04 +0700

    Custom Exception handling

---
+++ src/main/java/com/example/springboot_blog_rest_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.springboot_blog_rest_api.exception;
+
+import com.example.springboot_blog_rest_api.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException, WebRequest webRequest) {
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), resourceNotFoundException.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
