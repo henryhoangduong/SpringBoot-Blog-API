83970591f8c8ac27bc428e4f30ea6a027e86ac86
henryhoangduong <henryhoangduong@gmail.com>
2025-02-10 13:12:03 +0700

    create user and role jpa entity

---
+++ src/main/java/com/example/springboot_blog_rest_api/entity/Role.java
@@ -0,0 +1,13 @@
+package com.example.springboot_blog_rest_api.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
---
+++ src/main/java/com/example/springboot_blog_rest_api/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.springboot_blog_rest_api.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id" , referencedColumnName = "id"))
+    private Set<Role> roles;
+}
