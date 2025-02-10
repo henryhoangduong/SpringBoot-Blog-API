acf43ec6a0cee9d9250b27868f8b32b9cc1bb87b
henryhoangduong <henryhoangduong@gmail.com>
2025-02-10 13:20:46 +0700

    role repository

---
+++ src/main/java/com/example/springboot_blog_rest_api/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.springboot_blog_rest_api.repository;
+
+import com.example.springboot_blog_rest_api.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
